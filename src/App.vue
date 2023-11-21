46d7d44082eae4e0037b6c9b5ba0cdb7c3f1bc28
Louis Castillo <87865123+castillosdev@users.noreply.github.com>
2023-11-21 19:09:02 +0000

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+  import { ref } from 'vue';
+  const counter = ref(0);
+</script>
+
+<template>
+        <div class="counter">
+          <div class="counterContent">
+              <h2>The current count is...</h2>
+              <h1>{{ counter }}</h1>
+              <button @click="counter++">+</button>
+              <button @click="counter--">-</button>
+          </div>
+        </div>
+</template>
+
+<style scoped>
+.counter{
+    text-align: center;
+    width: 100vw;
+    height: 100vh;
+    background-color: aliceblue;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
